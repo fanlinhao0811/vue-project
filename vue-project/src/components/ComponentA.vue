b438bff988da290d6a31614ed4a397ab674391c0
fanlinhao0811 <haohaoangle@163.com>
2018-03-12 22:46:56 +0800

    0312 vue

---
+++ vue-project/src/components/ComponentA.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      {{$store.state.count}}
+			{{count1}}
+      <!-- <button @click="$store.commit('add',10)">add</button>
+      <button @click="$store.commit('reduce')">reduce</button> -->
+      <button @click="add(10)">add</button>
+      <button @click="reduce()">reduce</button>
+  </div>
+</template>
+<script>
+import ComponentB from "./ComponentB"
+import ComponentC from "./ComponentC"
+import {mapState,mapMutations} from 'vuex';
+export default {
+    data(){
+        return {
+            c: '1111'
+        }
+		},
+		methods:mapMutations(['add','reduce']),
+    // methods:{
+    //     change(arg){
+    //         this.c = arg;
+    //     }
+		// },
+		computed:mapState(["count","count1"]),
+    components:{
+    ComponentB,
+    ComponentC
+  }
+}
+</script>
+<style>
+
+</style>
---
+++ vue-project/src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './vuex/store'
+
+Vue.config.productionTip = false
+require("./assets/js/rem.js");
+require("./assets/css/reset.css");
+global.API_PROXY = 'https://bird.ioliu.cn/v1/?url='
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+	router,
+	store,
+  template: '<App/>',
+  components: { App }
+})
